5e7d6b44115bff2b6a428f08d8ad7ae27b33c7d0
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-07 15:07:19 +0530

    ability to delete first element from list

---
+++ LinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAsign
+{
+        public class LinkedList
+        {
+        public Node head; //new 
+        public void Add(int data)
+            {
+                Node node = new Node(data);
+                if (this.head == null)
+                    this.head = node;
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine("{0} inserted into the linked list", node.data);
+            }
+
+        public void Display()
+            {
+                Node temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("Linked list is empty");
+                    return;
+                }
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next; //temp=null
+                }
+            }
+        public Node DeleteFirstNode()
+            {
+                if (this.head == null)
+                    return null;
+                this.head = (Node)this.head.next;
+                return this.head;
+
+            }
+        }
+}
+
