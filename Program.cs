02906c3ec2cff610d9be77e2beaed994d0c183b5
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-07 15:50:56 +0530

    ability to create ordered list in ascending order 30,40,56,70

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace DataStructueAsign
+﻿using DataStructureAsign;
+
+namespace DataStructueAsign
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("welcome to data structure practice problem");
+            LinkedList objLinkedList = new LinkedList();
+            objLinkedList.Add(56);
+            objLinkedList.Add(30);
+            objLinkedList.Add(40);
+            objLinkedList.Add(70);
+            objLinkedList.Display();
+            objLinkedList.sortList();
+            objLinkedList.Display();
+
+
+            Console.ReadLine();
         }
     }
 }
