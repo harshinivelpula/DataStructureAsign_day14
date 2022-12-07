ec51ddf4ddeba48cec42c0cf75bfc8f851af46a3
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-07 15:39:56 +0530

    ability to delete 40 from list & show size is 3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace DataStructueAsign
+﻿using DataStructureAsign;
+
+namespace DataStructueAsign
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("welcome to data structure practice problem");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(40);
+            list.Add(70);
+            list.Display();
+            Console.Write("enter element to delete");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter previous element of that element to delete");
+            int b = Convert.ToInt32(Console.ReadLine());
+            list.Delete(b, a);
+            list.Display();
+            list.size();
         }
     }
 }
