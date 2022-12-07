393f9ddd16a2e7a52c416554a3049c55d6cfff5a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-07 13:38:02 +0530

    ability to create linkedlist 56,30,70

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace DataStructueAsign
+﻿using DataStructureAsign;
+
+namespace DataStructueAsign
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("welcome to data structure practice problem");
+            LinkedList List = new LinkedList();
+            List.add(70);
+            List.add(30);
+            List.add(56);
+            List.Display();
         }
     }
 }
